eb11d139875742da3f43f3567c6bb37b8bf226bf
korchizhinskiy <korchizhinskiy@gmail.com>
2024-09-28 16:33:43 +0700

    feat: update provider for sqlalchemy session

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,3 +1,5 @@
+from collections.abc import AsyncGenerator
+
 from dishka import (
     Provider,
     Scope,
@@ -6,8 +8,12 @@ from dishka import (
 )
 from dishka.integrations.fastapi import setup_dishka
 from fastapi.applications import FastAPI
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio.engine import AsyncEngine, create_async_engine
+from sqlalchemy.ext.asyncio.session import async_sessionmaker
 
 from app.config import Settings
+from app.database import get_connection_url
 
 
 class DishkaProvider(Provider):
@@ -18,6 +24,25 @@ class DishkaProvider(Provider):
     @provide(scope=Scope.APP)
     async def get_settings(self) -> Settings:
         return self.config
+
+    @classmethod
+    @provide(scope=Scope.APP)
+    async def engine(cls, settings: Settings) -> AsyncEngine:
+        database_url = get_connection_url(settings)
+        database_params = {}
+        return create_async_engine(database_url, **database_params)
+
+    @classmethod
+    @provide(scope=Scope.REQUEST)
+    async def get_session(cls, engine: AsyncEngine) -> AsyncGenerator[AsyncSession, None]:
+        async_session = async_sessionmaker(
+            engine,
+            class_=AsyncSession,
+            expire_on_commit=False,
+            autoflush=False,
+        )
+        async with async_session() as session:
+            yield session
 
 
 def init_di(app: FastAPI) -> None:
