9781546637a045365d519bf616daceb776118137
korchizhinskiy <korchizhinskiy@gmail.com>
2024-09-29 13:23:12 +0700

    fix: change import for settings class

--- app/infrastructure/alembic/env.py
+++ app/infrastructure/alembic/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from app.config import Settings
+from app.infrastructure.config import Settings
 from app.infrastructure.database import get_connection_url
 from app.infrastructure.models import Base
 
