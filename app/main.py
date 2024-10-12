3efae098c6adef44e2ce9f53dab795c94a39904b
korchizhinskiy <korchizhinskiy@gmail.com>
2024-10-12 18:11:32 +0700

    feat: run logging config function in main layer

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi.applications import FastAPI
 
 from app.infrastructure.ioc.dependencies import init_di
+from app.infrastructure.log import configure_logging
 from app.presentation.api.routers.auth import router as auth_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
 init_di(app)
+
+configure_logging()
