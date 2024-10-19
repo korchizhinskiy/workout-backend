bbedb776aae6ed8b22799c21c3a160caf72b4055
korchizhinskiy <korchizhinskiy@gmail.com>
2024-10-19 16:43:15 +0700

    feat: add encoding jwt in login interactor

--- app/auth/application/interactors/user_login.py
+++ app/auth/application/interactors/user_login.py
@@ -1,24 +1,34 @@
+import time
 from logging import getLogger
 
 import bcrypt
+import jwt
+from dishka import FromDishka
 
 from app.auth.application.dto.login import UserLoginDTO
+from app.auth.application.exceptions.user import WrongPasswordError
 from app.auth.application.interfaces.repository.user import IUserRepository
-from dishka import FromDishka
-import jwt
-
 from app.infrastructure.config import Settings
 
 log = getLogger(__name__)
 
 
 class UserLoginInteractor:
-    def __init__(self, repository: IUserRepository, settings: FromDishka[Settings]) -> str:
+    def __init__(self, repository: IUserRepository, settings: FromDishka[Settings]) -> None:
         self.repository = repository
         self.settings = settings
 
-    async def execute(self, user_dto: UserLoginDTO) -> None:
-        hashed_password = await self.repository.get_user_hashed_password(user_dto.username)
+    async def execute(self, user_dto: UserLoginDTO) -> str:
+        user = await self.repository.get_one(user_dto)
 
-        if bcrypt.checkpw(user_dto.password, hashed_password):
-            log.debug("User with username=%s sent right password.", user_dto.username)
+        if not bcrypt.checkpw(user_dto.password, user.password):
+            raise WrongPasswordError
+        # TODO: Set in service layer
+
+        token = jwt.encode(
+            payload={"user_id": str(user.id), "expires": time.time() + 100},
+            key=self.settings.certs.private_key,
+            algorithm=self.settings.certs.algorithm,
+        )
+        # TODO: Use Token DTO
+        return token
