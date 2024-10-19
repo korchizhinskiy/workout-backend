8837847f6b07d71159fe2c555d29888e71a713aa
korchizhinskiy <korchizhinskiy@gmail.com>
2024-10-19 16:39:05 +0700

    feat: provide settings into interactor

--- app/auth/application/interactors/user_login.py
+++ app/auth/application/interactors/user_login.py
@@ -4,13 +4,18 @@ import bcrypt
 
 from app.auth.application.dto.login import UserLoginDTO
 from app.auth.application.interfaces.repository.user import IUserRepository
+from dishka import FromDishka
+import jwt
+
+from app.infrastructure.config import Settings
 
 log = getLogger(__name__)
 
 
 class UserLoginInteractor:
-    def __init__(self, repository: IUserRepository) -> None:
+    def __init__(self, repository: IUserRepository, settings: FromDishka[Settings]) -> str:
         self.repository = repository
+        self.settings = settings
 
     async def execute(self, user_dto: UserLoginDTO) -> None:
         hashed_password = await self.repository.get_user_hashed_password(user_dto.username)
--- app/infrastructure/ioc/providers/auth/interactor.py
+++ app/infrastructure/ioc/providers/auth/interactor.py
@@ -5,6 +5,7 @@ from dishka.provider import Provider
 from app.auth.application.interactors.user_login import UserLoginInteractor
 from app.auth.application.interactors.user_registration import UserRegistrationInteractor
 from app.auth.infrastructure.repository.user import UserRepository
+from app.infrastructure.config import Settings
 
 # TODO: Set providers by modules
 
@@ -17,5 +18,5 @@ class InteractorProvider(Provider):
 
     @classmethod
     @provide(scope=Scope.REQUEST)
-    async def provide_user_login_interactor(cls, repository: UserRepository) -> UserLoginInteractor:
-        return UserLoginInteractor(repository)
+    async def provide_user_login_interactor(cls, repository: UserRepository, settings: Settings) -> UserLoginInteractor:
+        return UserLoginInteractor(repository, settings)
