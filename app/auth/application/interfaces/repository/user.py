1691ecc878f8effad5687fb9ee655dc6ce91bbaf
korchizhinskiy <korchizhinskiy@gmail.com>
2024-10-19 16:36:36 +0700

    feat: add get_one user function into user repository

--- app/auth/application/interfaces/repository/user.py
+++ app/auth/application/interfaces/repository/user.py
@@ -1,7 +1,9 @@
 from typing import Protocol
 from uuid import UUID
 
+from app.auth.application.dto.login import UserLoginDTO
 from app.auth.application.dto.registration import UserRegistrationDTO
+from app.auth.application.dto.user import UserDTO
 
 type HashPassword = bytes
 type UserId = UUID
@@ -9,6 +11,8 @@ type UserUsername = str
 
 
 class IUserRepository(Protocol):
+    async def get_one(self, user_dto: UserLoginDTO) -> UserDTO: ...
+
     async def create(self, user_dto: UserRegistrationDTO) -> None: ...
 
     async def get_user_hashed_password(self, username: UserUsername) -> bytes: ...
--- app/auth/infrastructure/repository/user.py
+++ app/auth/infrastructure/repository/user.py
@@ -4,7 +4,9 @@ from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.sql import select
 from sqlalchemy.sql.expression import exists
 
+from app.auth.application.dto.login import UserLoginDTO
 from app.auth.application.dto.registration import UserRegistrationDTO
+from app.auth.application.dto.user import UserDTO
 from app.auth.application.exceptions.user import UserNotFoundError
 from app.auth.infrastructure.models.user import User
 
@@ -16,6 +18,14 @@ type UserUsername = str
 class UserRepository:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
+
+    async def get_one(self, user_dto: UserLoginDTO) -> UserDTO:
+        user_query = select(User).where(User.username == user_dto.username)
+        user = await self.session.scalar(user_query)
+
+        if not user:
+            raise UserNotFoundError(username=user_dto.username)
+        return UserDTO.model_validate(user)
 
     async def create(self, user_dto: UserRegistrationDTO) -> None:
         user = User(**user_dto.model_dump())
