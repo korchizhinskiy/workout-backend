a20423ce1baadf19ceeff4ec6cccfa5b598c419e
Nazar Korchizhinskiy <korchizhinskiy@gmail.com>
2024-12-15 22:15:59 +0700

    feat: update user and auth session models, create compliant migrations

--- app/auth/infrastructure/models/session.py
+++ app/auth/infrastructure/models/session.py
@@ -1,9 +1,19 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+from uuid import UUID
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql.schema import ForeignKey
 
 from app.auth.infrastructure.models.base import Base
 
+if TYPE_CHECKING:
+    from app.auth.infrastructure.models.user import User
+
 
 class AuthSession(Base):
-    __tablename__ = "auth_sessions"
+    __tablename__ = "auth_session"
 
-    jwt: Mapped[str] = mapped_column(primary_key=True)
+    token: Mapped[str] = mapped_column(primary_key=True)
+
+    user: Mapped["User"] = relationship(cascade="all, delete", back_populates="auth_sessions")
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
--- app/auth/infrastructure/models/user.py
+++ app/auth/infrastructure/models/user.py
@@ -1,14 +1,18 @@
+from typing import TYPE_CHECKING
 from uuid import UUID
 
-from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import mapped_column, relationship
 from sqlalchemy.orm.base import Mapped
 from uuid6 import uuid7
 
 from app.auth.infrastructure.models.base import Base
 
+if TYPE_CHECKING:
+    from app.auth.infrastructure.models.session import AuthSession
+
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid7)
     username: Mapped[str] = mapped_column(unique=True)
@@ -17,3 +21,5 @@ class User(Base):
     first_name: Mapped[str]
     last_name: Mapped[str]
     second_name: Mapped[str | None]
+
+    auth_sessions: Mapped[list["AuthSession"]] = relationship(back_populates="user")
