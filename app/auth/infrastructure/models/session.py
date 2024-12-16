029b6df6bb2d113f220e0416e67e51db334737e6
Nazar Korchizhinskiy <korchizhinskiy@gmail.com>
2024-12-16 22:13:14 +0700

    refactor: add uuid for session model

--- app/auth/infrastructure/models/session.py
+++ app/auth/infrastructure/models/session.py
@@ -3,6 +3,7 @@ from uuid import UUID
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql.schema import ForeignKey
+from uuid6 import uuid7
 
 from app.auth.infrastructure.models.base import Base
 
@@ -13,7 +14,8 @@ if TYPE_CHECKING:
 class AuthSession(Base):
     __tablename__ = "auth_session"
 
-    token: Mapped[str] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid7)
+    token: Mapped[str]
 
     user: Mapped["User"] = relationship(cascade="all, delete", back_populates="auth_sessions")
     user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
