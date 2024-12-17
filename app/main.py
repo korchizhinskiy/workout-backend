1cd86b399275a51a5df9ed0eaca5d1a9c6b4c066
Nazar Korchizhinskiy <korchizhinskiy@gmail.com>
2024-12-17 14:08:19 +0700

    feat: add endpoint for get user profile
     - Check for user authentication;
     - Add response model for user profile model serialization.

--- app/main.py
+++ app/main.py
@@ -4,10 +4,14 @@ from app.auth.presentation.api.routers.auth import router as auth_router
 from app.auth.presentation.exceptions import setup_exception_handlers
 from app.infrastructure.ioc.dependencies import init_di
 from app.infrastructure.log import configure_logging
+from app.user.presentation.api.routers.user_profile import router as user_router
+
+# TODO: Set main router by package layer (in init module)
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(user_router)
 setup_exception_handlers(app)
 init_di(app)
 
