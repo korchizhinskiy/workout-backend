616c3c7d9d6f9ae2cc9b37817c1f17c89d612ed8
Nazar Korchizhinskiy <korchizhinskiy@gmail.com>
2024-12-18 15:57:12 +0700

    feat: add migration for trianing models and update metadata collection

--- app/infrastructure/alembic/env.py
+++ app/infrastructure/alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import engine_from_config, pool
 from app.auth.infrastructure.models.base import Base as AuthBase
 from app.infrastructure.config import Settings
 from app.infrastructure.database import get_connection_url
+from app.training.infrastructure.models.base import Base as TrainingBase
 
 config = context.config
 settings = Settings()  # type: ignore [reportCallIssue]
@@ -14,7 +15,7 @@ config.set_main_option("sqlalchemy.url", f"{get_connection_url(settings)}?async_
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-target_metadata = [AuthBase.metadata]
+target_metadata = [AuthBase.metadata, TrainingBase.metadata]
 
 
 def run_migrations_offline() -> None:
