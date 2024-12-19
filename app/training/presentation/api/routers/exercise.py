3129e4967f125511c2f98278df5f8aae8db8fbed
Nazar Korchizhinskiy <korchizhinskiy@gmail.com>
2024-12-19 13:17:16 +0700

    feat: add endpoint for get list of exercises

--- app/training/presentation/api/routers/exercise.py
+++ app/training/presentation/api/routers/exercise.py
@@ -8,10 +8,19 @@ from fastapi.routing import APIRouter
 from app.auth.application.dto.user import UserDTO
 from app.auth.infrastructure.dependencies import get_authenticated_user
 from app.training.application.dto.exercise import ExerciseDTO
-from app.training.application.interface.query.exercise import IExerciseQuery
+from app.training.application.interface.query.exercise import IExerciseListQuery, IExerciseQuery
 from app.training.presentation.schemas.exercise import ExerciseOutputSchema
 
 router = APIRouter(tags=["Exercise"], prefix="/training")
+
+
+@router.get("/exercises", response_model=list[ExerciseOutputSchema])
+@inject
+async def get_exercises(
+    idp: Annotated[UserDTO, Depends(get_authenticated_user)],
+    query: FromDishka[IExerciseListQuery],
+) -> list[ExerciseDTO]:
+    return await query.execute(idp)
 
 
 @router.get("/exercises/{exercise_id}", response_model=ExerciseOutputSchema)
@@ -21,4 +30,4 @@ async def get_exercise_by_id(
     idp: Annotated[UserDTO, Depends(get_authenticated_user)],
     query: FromDishka[IExerciseQuery],
 ) -> ExerciseDTO:
-    return (await query.execute(exercise_id, idp))
+    return await query.execute(exercise_id, idp)
