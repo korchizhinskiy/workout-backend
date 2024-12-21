469a15d48133424df75e70c4fc857ec042e12bc1
Nazar Korchizhinskiy <korchizhinskiy@gmail.com>
2024-12-21 16:37:12 +0700

    refactor: move funciton for hash password in security module

--- app/auth/application/interactors/user_registration.py
+++ app/auth/application/interactors/user_registration.py
@@ -1,8 +1,7 @@
-import bcrypt
-
 from app.auth.application.dto.registration import UserRegistrationDTO
 from app.auth.application.exceptions.user import UserAlreadyRegisteredError
 from app.auth.application.interfaces.repository.user import IUserRepository
+from app.auth.application.services.security import hash_password
 
 
 class UserRegistrationInteractor:
@@ -15,15 +14,8 @@ class UserRegistrationInteractor:
         if registered_user:
             raise UserAlreadyRegisteredError(username=user_dto.username)
 
-        hashed_password = self.hash_password(user_dto.password)
+        hashed_password = hash_password(user_dto.password)
 
         await self.repository.create(
             UserRegistrationDTO(**user_dto.model_dump(exclude={"password"}), password=hashed_password),
         )
-
-    # TODO: Set function in another module (may be using on registration, on login and change password)
-    # https://github.com/korchizhinskiy/workout-backend/issues/12
-    @staticmethod
-    def hash_password(password: bytes) -> bytes:
-        salt = bcrypt.gensalt()
-        return bcrypt.hashpw(password, salt)
